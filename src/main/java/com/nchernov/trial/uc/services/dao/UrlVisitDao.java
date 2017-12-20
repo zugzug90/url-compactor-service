b4b2d272e8bb90d486b7f18c309cb37861545479
Chernov <NAChernov@DBDC.luxoft.com>
2017-12-20 17:46:41 +0300

    An attempt to create top N visited report calculation via Named Native Query. TODO: finish it or accomodate better approach.

--- src/main/java/com/nchernov/trial/uc/services/dao/UrlVisitDao.java
+++ src/main/java/com/nchernov/trial/uc/services/dao/UrlVisitDao.java
@@ -1,6 +1,7 @@
 package com.nchernov.trial.uc.services.dao;
 
 import com.nchernov.trial.uc.domain.UrlVisit;
+import com.nchernov.trial.uc.domain.UrlVisitSummary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,7 +10,12 @@ import java.util.Collection;
 public interface UrlVisitDao extends JpaRepository<UrlVisit, Long> {
 
     String Q_GET_ALL_VISITS = "select uv from UrlVisit uv join uv.urlMapping";
+    String Q_GET_TOP_N_VISITED = "select url, pseudo_hash as pseudoHash from url_mapping, (select count(*) as visitCount, id AS url_visit_id FROM url_visit WHERE  GROUP BY id ORDER BY COUNT(*) DESC LIMIT ?1)";
+    //TODO: finish this query: how to join tables using fk ?? Where do I get those fk ??
 
     @Query(Q_GET_ALL_VISITS)
     Collection<UrlVisit> getAllVisits();
+
+    @Query(value = Q_GET_TOP_N_VISITED, nativeQuery = true)
+    Collection<UrlVisitSummary> getTopVisitedUrls(int count);
 }
--- src/main/java/com/nchernov/trial/uc/services/impl/CachingUrlVisitAnalyticsService.java
+++ src/main/java/com/nchernov/trial/uc/services/impl/CachingUrlVisitAnalyticsService.java
@@ -2,16 +2,23 @@ package com.nchernov.trial.uc.services.impl;
 
 import com.nchernov.trial.uc.domain.UrlVisitSummary;
 import com.nchernov.trial.uc.services.UrlVisitAnalyticsService;
+import com.nchernov.trial.uc.services.dao.UrlVisitDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
-import java.util.Collections;
 
 @Component
 public class CachingUrlVisitAnalyticsService implements UrlVisitAnalyticsService {
 
+    private UrlVisitDao urlVisitDao;
+
+    public CachingUrlVisitAnalyticsService(@Autowired UrlVisitDao urlVisitDao) {
+        this.urlVisitDao = urlVisitDao;
+    }
+
     @Override
     public Collection<UrlVisitSummary> topVisited(int count) {
-        return Collections.emptyList();
+        return urlVisitDao.getTopVisitedUrls(count);
     }
 }
